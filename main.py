1712236fa0e40406ac7ad96c1a0788699afcb0b3
su1jun <qaxsd530@gmail.com>
2024-05-15 00:00:12 +0900

    1

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ app.include_router(my_rounter)
 app.include_router(ws_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=False)
